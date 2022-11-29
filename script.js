a5f4a5c5a6da1ac70ac47c090a92255af163c8d1
Sam-WebP <113742608+Sam-WebP@users.noreply.github.com>
2022-11-29 17:35:57 +1100

    The slider is now dynamically updating the size of the grid

--- script.js
+++ script.js
@@ -10,36 +10,79 @@ const masterContainer = document.querySelector('#master-container');
 //Creating the slider
 let slider = document.getElementById("myRange");
 let sliderOutput = 16
+let gridSize
+
+let boxContain = document.createElement('div')
+let box = document.createElement('div')
 
 slider.oninput = function() {
     sliderOutput = this.value;
+    createGridContainer(sliderOutput);
   }
 
 //function that creates 16x16 grid on load of the window
 window.onload = function() {
-    createGridContainer();
+    firstLoad();
   };
   
-function createGridContainer () {
-// Creates box containers inside of the master container for the boxes to go into
-    for (let i = 1; i <= 5; i++) {
+function createGridContainer (gridSize) {
+   
+// Clear any existing boxes & containers before creating the next batch    
 
-    const boxContain = document.createElement('div')
+document.querySelectorAll(".containClass").forEach(e => e.remove());
+
+// Creates box containers inside of the master container for the boxes to go into
+    for (let i = 1; i <= gridSize; i++) {
+
+     boxContain = document.createElement('div')
         boxContain.style.display = 'flex';
         boxContain.style.border = '0px solid';
         boxContain.style.width = '352px'; //length of each row
         boxContain.style.height = '70.4px'; //height of each row should equal i/width
+        boxContain.classList.add('containClass');
         masterContainer.appendChild(boxContain)
 
 // Creates boxes and puts them into the container
-    for (let i = 1; i <= 5; i++) {
+    for (let i = 1; i <= gridSize; i++) {
 
-    let box = document.createElement('div');
+     box = document.createElement('div');
         box.style.display = 'flex';
         box.style.flexGrow = '1';
         box.style.padding = 'auto';
         box.style.border = '1px solid grey';
         box.style.backgroundColor = 'black';
+        box.classList.add('boxClass');
+        boxContain.appendChild(box);
+
+}
+}
+}
+
+/////////////////
+
+function firstLoad() {
+
+// Creates box containers inside of the master container for the boxes to go into
+    for (let i = 1; i <= 16; i++) {
+
+     boxContain = document.createElement('div')
+        boxContain.style.display = 'flex';
+        boxContain.style.border = '0px solid';
+        boxContain.style.width = '352px'; //length of each row
+        boxContain.style.height = '70.4px'; //height of each row should equal i/width
+        boxContain.classList.add('containClass');
+        masterContainer.appendChild(boxContain)
+
+// Creates boxes and puts them into the container
+    for (let i = 1; i <= 16; i++) {
+
+     box = document.createElement('div');
+        box.style.display = 'flex';
+        box.style.flexGrow = '1';
+        box.style.padding = 'auto';
+        box.style.border = '1px solid grey';
+        box.style.backgroundColor = 'black';
+        box.classList.add('boxClass');
         boxContain.appendChild(box);
 
 }
